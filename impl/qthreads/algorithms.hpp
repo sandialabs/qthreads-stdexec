71585137130fb33a1b90e221cd9a6b64f60bf839
Ian Henriksen <iandhenriksen@gmail.com>
2025-06-25 12:59:08 -0600

    Fix compilation.

--- impl/qthreads/algorithms.hpp
+++ impl/qthreads/algorithms.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <qthreads/algorithms/then_qthreads.hpp>
 #include <reference/algorithms/retry.hpp>
 #include <reference/algorithms/then.hpp>
-#include <reference/algorithms/then_qthreads.hpp>
 #include <reference/common_recv/expect_recv.hpp>
 

93d9e55a3992856c029e8add15edc136ec4e3393
Ian Henriksen <iandhenriksen@gmail.com>
2025-07-03 13:25:21 -0600

    Add a helper function for applying an operation many times by zipping arguments together from multiple tuples.

--- impl/qthreads/stdexec.hpp
+++ impl/qthreads/stdexec.hpp
@@ -30,6 +30,39 @@ struct is_index_sequence_impl<std::index_sequence<I...>> {
 template <typename T>
 concept is_index_sequence = is_index_sequence_impl<T>::val;
 
+// helper for apply_across
+template <std::size_t I, typename Func, typename... Args>
+decltype(auto) apply_at_index(Func &&func, Args &&...args) {
+  return static_cast<Func &&>(func)(std::get<I>(static_cast<Args &&>(args))...);
+}
+
+// helper for apply_across
+template <std::size_t... I, typename Func, typename... Args>
+decltype(auto)
+apply_across_impl(std::index_sequence<I...>, Func &&func, Args &&...args) {
+  // deliberately decaying to (possibly const) lvalue references by
+  // not forwarding since the function call potentially happens many times.
+  return std::make_tuple(apply_at_index<I>(args...)...);
+}
+
+// apply_across is like std::apply, but applies an operation
+// taking its arguments from each element of multiple tuples.
+// TODO: better argument type checking to make sure we're actually
+// getting tuple_like objects.
+// TODO: forward noexcept specifier.
+template <typename Func, typename... Args>
+decltype(auto) apply_across(Func &&func, Args &&...args) {
+  static_assert(sizeof...(Args),
+                "Ambiguous return size for tuple. At least one argument to map "
+                "onto is required.");
+  constexpr std::size_t tuple_size = std::tuple_size_v<Args...[0]>;
+  static_assert(((tuple_size == std::tuple_size_v<Args>) && ...),
+                "Tuple-likes to be mapped onto must all be the same size.");
+  return apply_across_impl(std::make_index_sequence<tuple_size>(),
+                           static_cast<Func &&>(func),
+                           static_cast<Args &&>(args)...);
+}
+
 struct qthreads_domain;
 struct qthreads_scheduler;
 struct qthreads_env;
